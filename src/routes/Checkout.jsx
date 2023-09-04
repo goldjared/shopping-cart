3cc175245a4e8fef7050b33dd913d2af453460b4
jared <114547220+goldjared@users.noreply.github.com>
2023-09-04 18:07:06 -0400

    removed unused import, remove Link in components

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-
 const Checkout = () => {
   return (
     <>
       <p>this checkout page</p>
-      <Link to="/">Click here to go back</Link>
     </>
   );
 };
