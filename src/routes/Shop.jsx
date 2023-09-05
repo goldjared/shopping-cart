1511bb7d25ac62563c7d32728e1a5a423a27996a
jared <jaredgold@proton.me>
2023-09-05 18:24:08 -0400

    make shop route render ShopItem components

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,7 +1,18 @@
+import ShopItem from "../components/ShopItem";
+
+const shopItemIDs = [1, 2, 3, 4,5,6,7,8,9,10];
+// const fetchedAlready = false;
+
 const Shop = () => {
+
   return (
     <>
       <p>this store page</p>
+      <div className="my-shop">
+        {shopItemIDs.map((item) => (
+          <ShopItem itemID={item} key={item}></ShopItem>
+        ))}
+      </div>
     </>
   );
 };
