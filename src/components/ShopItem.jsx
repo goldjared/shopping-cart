fe55e5e53f6042c623fdf74ccb032883f0d3d232
jared <114547220+goldjared@users.noreply.github.com>
2023-09-16 18:22:30 -0400

    format w/ prettier, refactor component

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { getProducts } from "../products";
 
 function ShopItem({ itemID, fn }) {
   const [price, setPrice] = useState(null);
@@ -6,39 +7,46 @@ function ShopItem({ itemID, fn }) {
   const [img, setImg] = useState(null);
   const [itemTitle, setItemTitle] = useState(null);
 
-
   useEffect(() => {
     const fetchShopItem = async () => {
-      const response = await fetch(`https://fakestoreapi.com/products/${itemID}`)
-      const data = await response.json();
+      const data = await getProducts(itemID);
       setItemTitle(data.title);
       setPrice(data.price);
-      setImg(data.image)
-      console.log('its fethcing...');
+      setImg(data.img);
       setLoading(false);
-
-    }
+    };
     fetchShopItem();
+  }, [itemID]);
 
-  }, [itemID])
-
-  if(loading) return <p>loading...</p>;
+  if (loading) return <p>loading...</p>;
 
   return (
     <>
-    <div className="shop-item">
-
-      <img src={img} alt=""/>
-      <div className="item-title">{itemTitle}</div>
-      <div className="item-price">${price}</div>
-      <form>
-        <label>quantity:<input type="number" name="quantity" id="quantity" max={99} min={1} /></label>
-        <input type="submit" onClick={fn} className="add-cart-btn" value="Add" />
-      </form>
-
-    </div>
+      <div className="shop-item">
+        <img src={img} alt="" />
+        <div className="item-title">{itemTitle}</div>
+        <div className="item-price">${price}</div>
+        <form>
+          <label>
+            quantity:
+            <input
+              type="number"
+              name="quantity"
+              id="quantity"
+              max={99}
+              min={1}
+            />
+          </label>
+          <input
+            type="submit"
+            onClick={fn}
+            className="add-cart-btn"
+            value="Add"
+          />
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export { ShopItem }
+export { ShopItem };
