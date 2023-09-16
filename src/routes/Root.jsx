08ef5547762eae5ef712212d0932e468c7c9e2fa
jared <114547220+goldjared@users.noreply.github.com>
2023-09-16 18:19:53 -0400

    make path names lowercase

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,4 @@
 import { Outlet, Link } from "react-router-dom";
-
 export default function Root() {
   return (
     <>
@@ -9,10 +8,10 @@ export default function Root() {
             <Link to={"/"}>Home</Link>
           </li>
           <li>
-            <Link to={`/Shop`}>Shop</Link>
+            <Link to={`/shop`}>Shop</Link>
           </li>
           <li>
-            <Link to={`/Checkout`}>Checkout</Link>
+            <Link to={`/cart`}>Cart</Link>
           </li>
         </ul>
       </nav>
