75f18ac2b5b52e236bd1408831d7bab515251ed6
jared <114547220+goldjared@users.noreply.github.com>
2023-09-19 21:09:55 -0400

    remove debug, format with prettier

--- src/products.js
+++ src/products.js
@@ -1,25 +1,34 @@
 let products = [];
-const shopItemIDs = [1, 2, 3];
+const shopItemIDs = [1, 2, 3, 4, 5];
+
+// const timer = ms => new Promise(res => setTimeout(res, ms))
+// shopProducts();
+
 function getProducts(id) {
   // if(products.length === 0) {
-  //   shopProducts();
+  // console.log(products, 'length 0 prods from getter');
+  // await timer(1000);
+
   // }
+  // console.log(products, 'prods');
   let selectedProduct;
   products.forEach((productObj) => {
     if (productObj.id === id) {
       selectedProduct = productObj;
     }
   });
+  // console.log(selectedProduct, 'here selected product');
   return selectedProduct;
 }
-
+// save the products into localstorage, then upon a refresh from any page. if localstorage, pull from ls.
+// then do not run shopProducts from app.jsx. (or, shopProd() can check if localStor, ? return : run )
 function shopProducts() {
   console.log("FIRED SHOPPRODUCTS");
   const fetchShopItem = async (itemID) => {
     const response = await fetch(`https://fakestoreapi.com/products/${itemID}`);
     const data = await response.json();
-    // console.log(data);
-    console.log("its fethcing...");
+    console.log(data, "DATA");
+    console.log("its fetching...");
     products.push({
       title: data.title,
       price: data.price,
@@ -32,4 +41,4 @@ function shopProducts() {
   }
 }
 
-export { shopProducts, getProducts };
+export { shopProducts, getProducts, shopItemIDs };
