33ddae563eafa66e8f11df54e01e671e19cdd8a4
jared <jaredgold@proton.me>
2023-09-19 21:03:42 -0400

    make shop item qty reset to 1 after click add

--- src/cartStorage.js
+++ src/cartStorage.js
@@ -4,9 +4,13 @@ let shoppingCart = [];
 function addToCartHandler(id, e) {
   e.preventDefault();
   const form = document.forms;
+
   let quantity = Number(form[id - 1].firstChild.lastChild.value);
 
   if (!quantity > 0) quantity = 1;
+
+  // reset form input field to 1 after clicking add to cart btn
+  form[id - 1].firstChild.lastChild.value = 1;
 
   let search = shoppingCart.find((item) => item.id === id);
   if (search) {
@@ -15,9 +19,9 @@ function addToCartHandler(id, e) {
     return;
   }
   let currentProductObj = getProducts(id);
+  // console.log(currentProductObj, 'currentPordObj');
   currentProductObj.quantity = quantity;
   shoppingCart.push(currentProductObj);
-  console.log(shoppingCart, "YO");
 }
 
 export { shoppingCart, addToCartHandler };
