2b17f9dae3660526dcf35b5691c0f339880a3427
jared <jaredgold@proton.me>
2023-09-28 14:36:07 -0400

    make base HTML more appealing

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
       <div className="page-text">
-        <h1>cool HOME PAGE</h1>
+        <h1>fake HOME PAGE</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, dolor
-          iure. Fugiat tempora fuga consequuntur ullam obcaecati dolorem nam
+          Welcome to my fake shopping cart. The purpose of this project
+          was my practice of React, <strong>click the <Link to={`/shop`}>shop</Link> link to leave this page.</strong> Fugiat tempora fuga consequuntur ullam obcaecati dolorem nam
+            
           fugit. Dolorum sit inventore iste aperiam, pariatur voluptatibus eum
           consequuntur adipisci! Accusamus exercitationem soluta iure, error qui
           officia reiciendis recusandae animi molestiae. At dignissimos alias
@@ -13,14 +16,7 @@ const Home = () => {
           sint debitis reprehenderit amet, ipsum beatae. Eum fugit architecto
           sit perferendis ipsam id qui dolor mollitia alias? Reprehenderit enim
           cupiditate voluptatibus quod quidem architecto, ut eum maxime eos
-          velit? Minima, sed cum id quasi odio sunt. Eius, velit id temporibus
-          suscipit quasi doloremque dolorum accusamus impedit repellat unde
-          laudantium incidunt, illo dolore dignissimos amet quod officiis
-          molestias ut? Vero rem sit quidem provident voluptas, animi ipsa?
-          Rerum labore consectetur culpa minima laboriosam deserunt, facilis
-          voluptatum alias hic eos nisi in harum neque quae perspiciatis quas
-          soluta ducimus dicta! Necessitatibus rem praesentium eligendi quas,
-          voluptatum debitis quaerat.
+          velit? Minima, sed cum id quasi odio sunt.
         </p>
       </div>
     </>
